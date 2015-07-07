f8d0bb20031a3e82c688d62b2309272bb4e256cb
Devin McGloin <devin@devinmcgloin.com>
2015-07-07 02:58:18 -0400

    Initial Commit

---
+++ haskell/rna-transcription/dna.hs
@@ -0,0 +1,10 @@
+module DNA (toRNA) where
+
+
+toRNA :: [Char] -> [Char]
+toRNA [] = []
+toRNA (x:xs)
+  | x == 'G' = 'C' : toRNA xs
+  | x == 'C' = 'G' : toRNA xs
+  | x == 'T' = 'A' : toRNA xs
+  | x == 'A' = 'U' : toRNA xs
